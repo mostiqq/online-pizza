c0ca338a6a8fcaf3512628946c5161b989cb64e6
mosty <100589608+mostiqq@users.noreply.github.com>
2024-08-05 15:51:07 +0800

    add changes in main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,5 @@
-import { Categories } from '@/components/shared/Categories'
 import { Container } from '@/components/shared/Container'
-import { SortPopup } from '@/components/shared/SortPopup'
+import { Filters } from '@/components/shared/Filters'
 import { Title } from '@/components/shared/Title'
 import { TopBar } from '@/components/shared/TopBar'
 
@@ -11,6 +10,16 @@ export default function Home() {
 				<Title text='Все пиццы' size='lg' className='font-extrabold' />
 			</Container>
 			<TopBar />
+			<Container className='pb-14 mt-10'>
+				<div className='flex gap-[60px]'>
+					<div className='w-[250px]'>
+						<Filters />
+					</div>
+					<div className='flex-1'>
+						<div className='flex flex-col gap-16'>Список товаров</div>
+					</div>
+				</div>
+			</Container>
 		</>
 	)
 }
